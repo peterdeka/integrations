6e0c1dc70e793aab3c071d702e6278f809bec256
Pietro De Caro <pietro.decaro@gmail.com>
2019-03-08 15:19:28 +0100

    updated ecs optimized amis

--- aws/ecs/packer/build-all-amis.sh
+++ aws/ecs/packer/build-all-amis.sh
@@ -7,17 +7,24 @@ if [ -z "${AWS_ACCESS_KEY_ID+x}" -a -z "${AWS_SECRET_ACCESS_KEY+x}" ]; then
 fi
 
 # Taken from http://docs.aws.amazon.com/AmazonECS/latest/developerguide/launch_container_instance.html
-BASE_AMIS=('us-east-1:ami-9eb4b1e5'
-	   'us-east-2:ami-1c002379'
-	   'us-west-1:ami-4a2c192a'
-	   'us-west-2:ami-1d668865'
-	   'eu-west-1:ami-8fcc32f6'
-	   'eu-west-2:ami-cb1101af'
-	   'eu-central-1:ami-0460cb6b'
-	   'ap-northeast-1:ami-b743bed1'
-	   'ap-southeast-1:ami-9d1f7efe'
-	   'ap-southeast-2:ami-c1a6bda2'
-	   'ca-central-1:ami-b677c9d2'
+BASE_AMIS=('us-east-2':'ami-0aa9ee1fc70e57450'
+    'us-east-1':'ami-007571470797b8ffa'
+    'us-west-2':'ami-0302f3ec240b9d23c'
+    'us-west-1':'ami-0935a5e8655c6d896'
+    'eu-west-2':'ami-0380c676fcff67fd5'
+    'eu-west-3':'ami-0b419de35e061d9df'
+    'eu-west-1':'ami-0b8e62ddc09226d0a'
+    'eu-central-1':'ami-01b63d839941375df'
+    'eu-north-1':'ami-03f8f3eb89dcfe553'
+    'ap-northeast-2':'ami-0c57dafd95a102862'
+    'ap-northeast-1':'ami-086ca990ae37efc1b'
+    'ap-southeast-2':'ami-0d28e5e0f13248294'
+    'ap-southeast-1':'ami-0627e2913cf6756ed'
+    'ca-central-1':'ami-0835b198c8a7aced4'
+    'ap-south-1':'ami-05de310b944d67cde'
+    'sa-east-1':'ami-09987452123fadc5b'
+    'us-gov-east-1':'ami-07dfc9cdc48d8649a'
+    'us-gov-west-1':'ami-914229f0'
 	  )
 
 # Mimic associative arrays using ":" to compose keys and values,
