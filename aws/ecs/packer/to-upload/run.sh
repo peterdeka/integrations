6a0bc8cc2ab47424e626c27d2fad6fb89d14d7ab
Pietro De Caro <pietro.decaro@gmail.com>
2019-04-11 15:16:03 +0200

    Added IPALLOC_RANGE environment variable parsing to run.sh

--- aws/ecs/packer/to-upload/run.sh
+++ aws/ecs/packer/to-upload/run.sh
@@ -6,8 +6,8 @@ LOGGER_TAG=""
 
 succeed_or_die() {
     if ! OUT=$("$@" 2>&1); then
-	echo "error: '$@' failed: $OUT" | logger -p local0.error -t $LOGGER_TAG
-	exit 1
+        echo "error: '$@' failed: $OUT" | logger -p local0.error -t $LOGGER_TAG
+        exit 1
     fi
     echo $OUT
 }
@@ -15,12 +15,16 @@ succeed_or_die() {
 run_weave() {
   # launch weave
   PEERS=$(succeed_or_die /etc/weave/peers.sh)
-  succeed_or_die weave launch --plugin=false --no-restart --hostname-from-label 'com.amazonaws.ecs.container-name' $PEERS
+  IPALLOC=""
+  if [[ -n "$IPALLOC_RANGE" ]]; then
+    IPALLOC="--ipalloc-range $IPALLOC_RANGE"
+  fi
+  succeed_or_die weave launch --plugin=false --no-restart --hostname-from-label 'com.amazonaws.ecs.container-name' $IPALLOC $PEERS
 }
 
 case $1 in
     weave)
-	LOGGER_TAG="weave_runner"
+        LOGGER_TAG="weave_runner"
         run_weave
         ;;
 esac
